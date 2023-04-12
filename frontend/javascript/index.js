6160348b713731bb39350274f20b9c44d03de2b1
Spike Ilacqua <spike@indra.com>
2023-04-12 17:46:31 -0600

    WIP

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,15 @@
-import "index.css"
-import "syntax-highlighting.css"
+import "index.css";
+import "syntax-highlighting.css";
+import "animate.css";
 
 // Import all JavaScript & CSS files from src/_components
-import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
 
-console.info("Bridgetown is loaded!")
+import WOW from "wowjs";
+
+window.wow = new WOW.WOW({
+  live: false,
+});
+
+import "./main";
+import "./scroll";
