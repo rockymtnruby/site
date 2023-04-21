65369ca5f4980c3b212562644881dc6fab490318
Spike Ilacqua <spike@indra.com>
2023-04-20 21:12:28 -0600

    WIP

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -1,5 +1,26 @@
 class Shared::Navbar < Bridgetown::Component
-  def initialize(metadata:, resource:)
-    @metadata, @resource = metadata, resource
+  def initialize(site:, metadata:, resource:)
+    @site, @metadata, @resource = site, metadata, resource
+  end
+
+  def homepage?
+    @resource.relative_url == '/'
+  end
+
+  def homepage_sections
+    @metadata.main_page.sections.select {|section| section.enabled && section.navbar }
+  end
+
+  def top_level_pages
+    @site.collections.pages.resources.select { |r| r.data.top_level }.sort_by { |r| r.data.title }
+  end
+
+  def scroll_class
+    homepage? ? 'ud-menu-scroll' : ''
+  end
+
+  def relative_href(section)
+    ref = section.slug || section.name
+    (homepage? ? '#' : '/#') +  ref
   end
 end
