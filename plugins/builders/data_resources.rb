c6fbeffacf1f3e4fbcc3dea5645fffe1c1a065f4
Spike Ilacqua <spike@indra.com>
2023-08-01 08:35:41 -0600

    The Program

---
+++ plugins/builders/data_resources.rb
@@ -0,0 +1,19 @@
+class Builders::DataResources < SiteBuilder
+  def build
+     hook :site, :post_read do
+       add_talks_resources
+     end
+  end
+
+
+  def add_talks_resources
+    name = "talks"
+    site.data.talks.each do |talk|
+      add_resource name, "#{Bridgetown::Utils.slugify(talk.title)}.md" do
+        ___ talk
+        layout :talk
+     end
+    end
+    site.collections[name].sort_resources!
+  end
+end
